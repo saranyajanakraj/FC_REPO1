5ec7f2fe15974b2131419e0a7ae7fd64d33359eb
Jenkins Automation Server <jenkins@ip-172-31-12-48.ap-south-1.compute.internal>
2022-07-26 06:52:50 +0000

    change 3

--- copy.sh
+++ copy.sh
@@ -1,6 +1,6 @@
-scp /var/lib/jenkins/demo/* ec2-user@13.235.70.132:/home/ec2-user/
-ssh ec2-user@13.235.70.132 'chmod 777 /home/ec2-user/backup.sh'
-ssh ec2-user@13.235.70.132 'chmod 777 /home/ec2-user/deploy.sh'
-ssh ec2-user@13.235.70.132 'chmod 777 /home/ec2-user/cicd.config'
-ssh ec2-user@13.235.70.132 '/home/ec2-user/backup.sh'
-ssh ec2-user@13.235.70.132 '/home/ec2-user/deploy.sh'
+scp /var/lib/jenkins/demo/* ec2-user@13.127.39.162:/home/ec2-user/
+ssh ec2-user@13.127.39.162 'chmod 777 /home/ec2-user/backup.sh'
+ssh ec2-user@13.127.39.162 'chmod 777 /home/ec2-user/deploy.sh'
+ssh ec2-user@13.127.39.162 'chmod 777 /home/ec2-user/cicd.config'
+ssh ec2-user@13.127.39.162 '/home/ec2-user/backup.sh'
+ssh ec2-user@13.127.39.162 '/home/ec2-user/deploy.sh'
